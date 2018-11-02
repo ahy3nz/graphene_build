04cb1a8666a5a9b86ef71c44865be41e0989de2c
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-11-02 16:58:59 -0500

    Better workflow

--- aggregation/pack_bulk.py
+++ aggregation/pack_bulk.py
@@ -17,27 +17,30 @@ header = """; Include forcefield parameters
 #include "/raid6/homes/ahy3nz/Trajectories/graphene_studies/graphene_files/force-field/molecules.itp"
 #include "/raid6/homes/ahy3nz/Trajectories/graphene_studies/graphene_files/force-field/ions.itp" """
 
+ingredients_folder = 'ingredients'
+out_folder = 'trial1'
 
 box = [10, 10, 10]
 cmpd = mb.Compound()
 sheet = 'sheet.gro'
-components = [ ('GRA_5', 1, 'sheet.gro', mb.load('sheet.gro')), 
-                ('DOPC', 100, 'dopc.gro', mb.load('dopc.gro')),
-                ('CHL1', 100, 'chl1.gro', mb.load('chl1.gro')),
-                ('SOL', 1000, 'SOL.gro', mb.load('SOL.gro')) ]
-outfile = 'bulk.gro'
+components = [ ('GRA_5', 1, 'sheet.gro', mb.load('{}/sheet.gro'.format(ingredients_folder))), 
+                ('DOPC', 100, 'dopc.gro', mb.load('{}/dopc.gro'.format(ingredients_folder))),
+                ('CHL1', 100, 'chl1.gro', mb.load('{}/chl1.gro'.format(ingredients_folder))),
+                ('SOL', 1000, 'SOL.gro', mb.load('{}/SOL.gro'.format(ingredients_folder))) ]
+outfile = '{}/bulk.gro'.format(out_folder)
+outtop = '{}/bulk.top'.format(out_folder)
 old_files = glob.glob('*bulk.gro*')
 for thing in old_files:
     os.remove(thing)
-with open('std.out','w') as stdoutfile, open('std.err', 'w') as stderrfile:
+with open('{}/build.out'.format(out_folder),'w') as stdoutfile, open('{}/build.err'.format(out_folder), 'w') as stderrfile:
     for i, (name, nmol, grofile, cmpd_i)  in enumerate(components):
         if i == 0:
-            cmd = ('gmx insert-molecules -ci sheet.gro -nmol 1 -o {0} -box '
-                '{1} {2} {3}'.format(outfile, *box))
+            cmd = ('gmx insert-molecules -ci {4}/sheet.gro -nmol 1 -o {0} -box '
+                '{1} {2} {3}'.format(outfile, *box, ingredients_folder))
         else:
-            cmd = ('gmx insert-molecules -f {0} -ci {1} '
+            cmd = ('gmx insert-molecules -f {0} -ci {6}/{1} '
                 '-nmol {2} -o {0} -box {3} {4} {5}'.format(outfile, grofile, 
-                    nmol, *box))
+                    nmol, *box, ingredients_folder))
         print(cmd)
         p = subprocess.Popen(cmd,
                 stdout=stdoutfile, stderr=stderrfile, shell=True)
@@ -47,4 +50,20 @@ with open('std.out','w') as stdoutfile, open('std.err', 'w') as stderrfile:
             to_add.name = name
             cmpd.add(to_add)
 
-bilayer.write_gmx_topology(cmpd, 'bulk.top', header=header)
+    bilayer.write_gmx_topology(cmpd, outtop, header=header)
+
+
+with open('{0}/em.out'.format(out_folder), 'w') as log:
+    p = subprocess.Popen('gmx grompp -f mdp/em.mdp -c {0} '
+            '-p {1} -o {2}/em'.format(outfile, outtop, out_folder),
+            shell=True, stdout=log, stderr=log)
+    p.wait()
+    p = subprocess.Popen('gmx mdrun -deffnm {0}/em'.format(out_folder), 
+            shell=True, stdout=log, stderr=log)
+    p.wait()
+
+with open('{0}/npt.out'.format(out_folder), 'w') as log:
+    p = subprocess.Popen('gmx grompp -f mdp/npt.mdp -c {1}/em.gro '
+            '-p {0} -o {1}/npt -maxwarn 1'.format(outtop, out_folder),
+            shell=True, stdout=log, stderr=log)
+    p.wait()
